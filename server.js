d02cc48b2280992caf9f09b6ed991ddc63eeaa04
AndreaParisiUnige <s5202353@studenti.unige.it>
2023-12-29 15:45:51 +0100

    Aggiunta a userGoal in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let userGoal = 'Learn Docker!';
+let userGoal = 'Learn Docker! And GitHub actions..';
 
 app.use(
   bodyParser.urlencoded({
